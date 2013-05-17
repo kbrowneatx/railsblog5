f7ecc0bc2954d77c3b00732d99c9fcc0797dd364
Kevin F Browne <kbrowneatx@gmail.com>
2013-05-17 10:28:34 -0500

    user views and controllers working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :user, :content, :title
+  attr_accessible :user_id, :content, :title
   
   belongs_to :user
   has_many :comments, :dependent => :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :email, :name, :password, :post_anonymously, :twitter, :role, :website
+  validates :name, :email, :presence => true
   
   ROLES = %w[author reader anon_reader]
   has_many :posts
   has_many :comments
+
+
 end
